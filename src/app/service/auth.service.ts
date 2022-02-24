b1d234082ffe4bcfc7a76295b0ebc552ed3c9c8f
Armando <armandomarinz@hotmail.com>
2022-02-24 17:42:15 -0300

    @ArmandoD3v [ Task : Login e Cadastro]

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../model/User';
+import { UserLogin } from '../model/UserLogin';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+  private htpp: HttpClient ) { }
+
+  entrar(userLogin: UserLogin): Observable<UserLogin>{
+return this.htpp.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
+  }
+
+  cadastrar(user:User): Observable<User>{
+return this.htpp.post<User>('http://localhost:8080/usuarios/cadastrar', user)
+  }
+}
